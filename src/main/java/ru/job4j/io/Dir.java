8ce5a687f08542efea2975608d52b2fe599d13e5
FredfromHell <bespalov@avtocenter.ru>
2022-03-24 10:26:46 +0300

    4.0. File [#252491]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -6,10 +6,12 @@ public class Dir {
     public static void main(String[] args) {
         File file = new File("c:\\projects\\testfile.txt");
         if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String
+                    .format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isFile()) {
-            throw new IllegalArgumentException(String.format("Not file %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String
+                    .format("Not file %s", file.getAbsoluteFile()));
         }
         System.out.println("Size" + " " + file.length());
         System.out.println("Name" + " " + file.getName());
