d94f56f72d07e1578d0d581800a0f06a8bfa3523
Fredfromhell <bespalov@avtocenter.ru>
2022-03-30 13:57:47 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,14 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".xml")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Не задан каталог поиска");
+        }
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Название файла");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
