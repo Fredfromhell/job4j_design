554592bbecb44ce5a6809c3031b887322b537a21
Fredfromhell <bespalov@avtocenter.ru>
2022-04-06 09:02:58 +0300

    4.2. Поиск дубликатов [#315066 #281849]

--- src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -14,12 +14,19 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (file.toFile().isFile()) {
-            if (rsl.get(keySearch(file)) == null) {
+            FileProperty keySearchRsl = null;
+            for (FileProperty fileProperty : rsl.keySet()) {
+                if (fileProperty.getName().equals(file.getFileName().toString())
+                        && fileProperty.getSize() == file.toFile().length()) {
+                    keySearchRsl = fileProperty;
+                }
+            }
+            if (rsl.get(keySearchRsl) == null) {
                 rsl.put(new FileProperty(file.toFile()
                         .length(), file.getFileName().toString()), new ArrayList<>(List
                         .of(file.toAbsolutePath())));
             } else {
-                rsl.get(keySearch(file)).add(file.toAbsolutePath());
+                rsl.get(keySearchRsl).add(file.toAbsolutePath());
 
             }
         }
@@ -30,23 +37,10 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
         List<Path> list = new ArrayList<>();
         for (FileProperty name : rsl.keySet()) {
             if (rsl.get(name).size() > 1) {
-                for (Path path : rsl.get(name)) {
-                    list.add(path.toAbsolutePath());
+                list.addAll(rsl.get(name));
                 }
             }
-        }
         return list;
-    }
-
-    public FileProperty keySearch(Path file) {
-        FileProperty keySearchRsl = null;
-        for (FileProperty fileProperty : rsl.keySet()) {
-            if (fileProperty.getName().equals(file.getFileName().toString())
-                    && fileProperty.getSize() == file.toFile().length()) {
-                keySearchRsl = fileProperty;
-            }
-        }
-        return keySearchRsl;
     }
 }
 
