40d6317bf06bfafa9e3c90c7619e69e332536141
Fredfromhell <bespalov@avtocenter.ru>
2022-04-06 11:28:37 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,53 @@
+package ru.job4j.io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (values.get(key) == null) {
+            throw new IllegalArgumentException("Ключ не существует");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        Arrays.stream(args).filter(this::checkString)
+                .map(e -> e.split("=", 2))
+                .forEach(s -> values.put(s[0].substring(1), s[1]));
+    }
+
+    public boolean checkString(String string) {
+        int count = 0;
+        int index = string.indexOf("=");
+        for (int i = 0; i < string.length(); i++) {
+            if (String.valueOf(string.charAt(i)).equals("=")) {
+                count++;
+            }
+        }
+        if (index == -1 || index == 0
+                || (count == 1 && string.endsWith("="))) {
+            throw new IllegalArgumentException("Нарушение шаблона файла");
+        }
+        return true;
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
