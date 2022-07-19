7d86596d549a14f87703e9e0d603c9840137798d
Fredfromhell <bespalov@avtocenter.ru>
2022-07-19 11:05:14 +0300

    0. Что такое база данных. [#6860]

---
+++ script.sql
@@ -0,0 +1,11 @@
+create table pc (
+id serial primary key,
+processor varchar(30),
+ram int,
+gpu text);
+
+insert into pc(processor, ram, gpu) values ('Intel core i7', '16', 'rtx 3090ti')
+
+update pc set ram = '32';
+
+delete from pc;
