b21fa2a26636b6631093fec2f4d5b5fdf6febdfd
Fredfromhell <bespalov@avtocenter.ru>
2022-07-19 11:40:34 +0300

    0. Что такое база данных. [#6860]

--- script.sql
+++ script.sql
@@ -4,7 +4,7 @@ processor varchar(30),
 ram int,
 gpu text);
 
-insert into pc(processor, ram, gpu) values ('Intel core i7', '16', 'rtx 3090ti')
+insert into pc(processor, ram, gpu) values ('Intel core i7', '16', 'rtx 3090ti');
 
 update pc set ram = '32';
 
