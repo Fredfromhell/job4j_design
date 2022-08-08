1efa73ceaceda1c542b61d80defd323e14299d77
FredfromHell <bespalov@avtocenter.ru>
2022-08-08 15:30:07 +0300

    1. Inner join [#373617 #329968]

--- 2.sql
+++ 2.sql
@@ -1,3 +1,24 @@
+create table flat(
+    id serial primary key,
+    number int
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255),
+    flat_id int references flat(id) unique
+);
+
+insert into flat(number) values (1);
+insert into flat(number) values (2);
+insert into flat(number) values (3);
+
+insert into people(name, flat_id) values ('Ivan', 1);
+insert into people(name, flat_id) values ('Boris', 2);
+insert into people(name, flat_id) values ('Petr', 3);
+insert into people(name) values ('Vasya');
+insert into people(name) values ('Anya');
+
 select * from people
 join flat on people.flat_id=flat.id;
 
