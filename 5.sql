3413b929ff8023d753be408344b9678761657c7b
FredfromHell <bespalov@avtocenter.ru>
2022-08-10 13:22:39 +0300

    3. Фильтры [#1732]

--- 5.sql
+++ 5.sql
@@ -15,9 +15,13 @@ insert into type (name) values ('Сыр'), ('Молоко');
 insert into product (name, type_id, expired_date, price) values ('Сыр моцарелла',5,'2022-9-10',500), ('Сыр гауда',5, '2022-7-10',300),
 ('Сыр тельзитир',5,'2022-10-10',400),('Мороженое лакомка',6,'2022-11-11', 100), ('Мороженое стаканчик',6, '2022-12-12',50), ('Молоко 10%',6, '2022-8-14', 80);
 
-select * from product where type_id=5; 
+select t.name, p.name from type t
+join product p on p.type_id=t.id where t.name = 'Сыр'  ;
+ 
 select * from product where name like 'Мороженое%'; 
-select * from product where expired_date < '2022.8.9';
+
+select * from product where expired_date < current_date;
+
 select  max(price) from product;
 
 select t.name, count(p.name)
@@ -26,7 +30,8 @@ join product as p
 on p.type_id=t.id
 group by t.name;
 
-select * from product where type_id=5 or type_id=6;
+select t.name, p.name from type t
+join product p on p.type_id=t.id where t.name = 'Сыр' or t.name = 'Молоко' ;
 
 select t.name, count(p.name)
 from type as t
